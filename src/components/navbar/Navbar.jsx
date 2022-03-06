c986c2906234113badef149c9c4923e2cbd45f73
ma9da <dodymasry25@gmail.com>
2022-03-06 20:36:20 +0200

    sytling added

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,24 +1,29 @@
 import React from 'react'
+import logo from  "../../assets/icons/logo.svg"
+import cart from "../../assets/icons/cart.svg"
 import "../../styles/style.css"
+import "./navbar.css"
 function Navbar() {
     return (
         <>
-            <header className="container flexContainer">
+            <header className="navbar container flexContainer">
                 <ul className="navbarLinks">
                     <li><button>men</button></li>
                     <li><button>women</button></li>
                     <li><button>kids</button></li>
                 </ul>
                 <figure className="navbarLogo">
-                    <img src="" alt="" />
+                    <img src={logo} alt="logo" />
                 </figure>
                 <div className="navbarIcons">
                     <select name="currency" id="currency">
-                        <option value="$">$ USD</option>
-                        <option value="€">€ EUR</option>
-                        <option value="¥">¥ JPY</option>
+                        <option value="$">$</option>
+                        <option value="€">€</option>
+                        <option value="¥">¥</option>
                     </select>
-                    <img src="" alt="" />
+                    <button>
+                    <img src={cart} alt="cart" />
+                    </button>
                 </div>
             </header>
         </>
